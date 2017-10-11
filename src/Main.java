22f98fddc62f0136bb41cfda3833f079ba9fbd4c
Roman0532 <Roman62477@yandex.ru>
2017-10-12 00:30:04 +0700

    create collections of users and adding test data

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 /**
  * Created by Roman Maximov on 11.10.2017
  */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User((long)1,"Roman","123"));
+        users.add(new User((long)2,"Roman1","000"));
     }
 }
