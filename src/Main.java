b12404323705b1a66ec59c5a2a94399ddbbe61ca
Roman0532 <Roman62477@yandex.ru>
2017-10-12 00:08:25 +0700

    Create class User

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+/**
+ * Created by Roman Maximov on 11.10.2017
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
---
+++ src/User.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Roman Maximov on 12.10.2017
+ */
+public class User {
+    private Long id;
+    private String login;
+    private String password;
+
+    public User(Long id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
