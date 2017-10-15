f11aaa081bb3708ec1e78f557528846b3ba781eb
Roman0532 <Roman62477@yandex.ru>
2017-10-15 18:34:22 +0700

    added checking role

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,22 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        boolean login = false;
+        boolean role = false;
         ArrayList<User> users = new ArrayList<User>();
         users.add(new User((long)1,"Roman","123"));
         users.add(new User((long)2,"Roman1","000"));
+        users.add(new User((long)3,"Roman2","0000"));
 
         ArrayList<Resourse> resourses = new ArrayList<Resourse>();
         resourses.add(new Resourse((long)1,"a.b"));
-        resourses.add(new Resourse((long)1,"AB"));
+        resourses.add(new Resourse((long)2,"AB"));
 
         ArrayList<UserRes> userRes = new ArrayList<UserRes>();
         userRes.add(new UserRes((long)1,(long)1,(long)1,Roles.READ.toString()));
         userRes.add(new UserRes((long)2,(long)2,(long)2,Roles.WRITE.toString()));
+        userRes.add(new UserRes((long)3,(long)3,(long)3,Roles.EXECUTE.toString()));
+
         if (args.length < 2) {
             System.out.println("Недостаточно параметров");
         }
@@ -28,16 +33,41 @@ public class Main {
             }
             for (int i = 0; i < users.size(); i++) {
                 if (args[0].equals(users.get(i).getLogin())) {
-                    if (args[1]!=(users.get(i).getPassword())) {
+                    if (!args[1].equals(users.get(i).getPassword())) {
                         System.exit(2);
                     }
                 }
             }
             for (int i = 0; i < users.size(); i++) {
-                if ((args[0]!=(users.get(i).getLogin()))) {
-                    System.exit(1);
+                if ((args[0].equals(users.get(i).getLogin()))) {
+                       login = true;
                 }
-
+            }  if (!login){System.exit(1);}
+        }
+        //Авторизация Проверка на запрашиваемую роль
+        if (args.length == 3){
+            for (int i = 0; i <userRes.size(); i++) {
+                for (int j = 0; j < users.size(); j++) {
+                    if (users.get(j).getId().equals(userRes.get(i).getUser_id()) && args[1].equals(users.get(j).getPassword()) && (args[0].equals(users.get(j).getLogin()) ))
+                    {
+                        if(args[2].equals(userRes.get(i).getRole()))
+                        {   role = true;
+                            System.exit(5);
+                        }
+                    }
+                }
+            } if (!role) {System.exit(3);}
+            for (int i = 0; i < users.size(); i++) {
+                if ((args[0].equals(users.get(i).getLogin()))) {
+                    login = true;
+                }
+            }  if (!login){System.exit(1);}
+            for (int i = 0; i < users.size(); i++) {
+                if (args[0].equals(users.get(i).getLogin())) {
+                    if (!args[1].equals(users.get(i).getPassword())) {
+                        System.exit(2);
+                    }
+                }
             }
         }
     }
