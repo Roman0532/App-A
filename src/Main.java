6503a5b525641805c93662d791b9ca9c4373ec6e
Roman0532 <Roman62477@yandex.ru>
2017-10-19 21:10:06 +0700

    add class validator

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,42 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 /**
  * Created by Roman Maximov on 11.10.2017
  */
 
 public class Main {
+
     //Авторизация
+    private static boolean chekValue(String voluve) {
+        return voluve.matches("^-?\\d+$");
+    }
+
+    private static boolean chekDate(String date) {
+        try {
+            LocalDate d = LocalDate.parse(date);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void isAccount(String ds, String de, String vol, int argslenght) {
+        boolean check;
+        boolean check1;
+        boolean check2;
+        if (chekDate(ds)) { check = true; } else { check = false; }
+        if (chekDate(de)) { check1 = true; } else { check1 = false; }
+        if (chekValue(vol)) { check2 = true; } else { check2 = false; }
+
+        if (!check) { System.exit(5); }
+        if (!check1) { System.exit(5); }
+        if (!check2) { System.exit(5); }
+    }
+
+
     private static void isAutoriz(String log, String pass, String rol, String res, ArrayList<User> users, ArrayList<Resourse> resourses, ArrayList<UserRes> userRes, int argslenght) {
         boolean role = false;
         boolean childRes = false;
@@ -28,41 +58,45 @@ public class Main {
             for (Resourse resourse : resourses) {
                 if (pass.equals(users.get(k).getPassword()) && (log.equals(users.get(k).getLogin()))) {
                     if (resourse.getId().equals(userRes.get(k).getRes_id())) {
-                        if (res.equals(resourse.getPath())) {
-                            path = true;
-                            System.exit(0);
-
-                        } else {
-                            StringTokenizer resUser = new StringTokenizer(resourse.getPath(), "\\.");
-                            StringTokenizer transfRes = new StringTokenizer(res, "\\.");
-                            String[] tRes = new String[transfRes.countTokens()];
-                            String[] rUser = new String[resUser.countTokens()];
-                            if (tRes.length < rUser.length) {
-                                System.exit(4);
-                            }
-                            for (int i = 0; i < rUser.length; i++) {
-                                rUser[i] = (String) resUser.nextToken();
-                            }
-                            for (int j = 0; j < tRes.length; j++) {
-                                tRes[j] = (String) transfRes.nextToken();
-                            }
-                            for (int i = 0; i < rUser.length; i++) {
-                                if (rUser[i].equals(tRes[i])) {
-                                    childRes = true;
-                                } else {
-                                    childRes = false;
-                                    count++;
+                        {
+                            if (res.equals(resourse.getPath())) {
+                                path = true;
+                                if (argslenght == 4) {
+                                    System.exit(0);
+                                }
+                            } else if (!path) {
+                                StringTokenizer resUser = new StringTokenizer(resourse.getPath(), "\\.");
+                                StringTokenizer transfRes = new StringTokenizer(res, "\\.");
+                                String[] tRes = new String[transfRes.countTokens()];
+                                String[] rUser = new String[resUser.countTokens()];
+                                if (tRes.length < rUser.length) {
+                                    System.exit(4);
+                                }
+                                for (int i = 0; i < rUser.length; i++) {
+                                    rUser[i] = (String) resUser.nextToken();
+                                }
+                                for (int j = 0; j < tRes.length; j++) {
+                                    tRes[j] = (String) transfRes.nextToken();
+                                }
+                                for (int i = 0; i < rUser.length; i++) {
+                                    if (rUser[i].equals(tRes[i])) {
+                                        path = true;
+                                    } else {
+                                        path = false;
+                                        count++;
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-            if (count >= 1) {
-                System.exit(4);
+                if (count >= 1) {
+                    System.exit(4);
+                }
             }
         }
     }
+
     //Аутентификация
     private static void isAuth(String log, String pass, ArrayList<User> users, int argslenght) {
         boolean login = false;
@@ -115,6 +149,13 @@ public class Main {
         if (args.length == 4) {
             isAuth(args[0], args[1], users, args.length);
             isAutoriz(args[0], args[1], args[2], args[3], users, resourses, userRes, argslenght);
+
+        }
+        if (args.length == 7) {
+            isAuth(args[0], args[1], users, args.length);
+            isAutoriz(args[0], args[1], args[2], args[3], users, resourses, userRes, argslenght);
+            isAccount(args[4], args[5], args[6], args.length);
         }
     }
 }
+
--- src/Test.bat
+++ src/Test.bat
@@ -23,4 +23,15 @@ java Main Roman1 000 WRITE ab.c
 echo "4" %ERRORLEVEL%
 java Main Roman1 000 WRITE a.QQ
 echo "4" %ERRORLEVEL%
+
+java Main Roman 123 READ a.b 2017-10-08 2017-10-08 100
+echo "0" %ERRORLEVEL%
+java Main Roman 123 READ a.b 1111111111 2017-10-08 100
+echo "5" %ERRORLEVEL%
+java Main Roman 123 READ a.b 2017-10-08 2017-10-08 str100
+echo "5" %ERRORLEVEL%
+java Main Roman 123 WRITE a.b 1111111111 2222222222 100
+echo "3" %ERRORLEVEL%
+java Main Vasya qwerty READ A.B 1111111111 2222222222 str100
+echo "1" %ERRORLEVEL%
 PAUSE
