079e42bffefc6eaa226efb15669cf1b73503b9ff
Roman0532 <Roman62477@yandex.ru>
2017-10-25 14:49:57 +0700

    add v 1.1

---
+++ src/methods/Methods.java
@@ -0,0 +1,198 @@
+package methods;
+
+import classes.Roles;
+import classes.User;
+import classes.UserRes;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Methods {
+
+    /**
+     * Коллекция пользователей
+     */
+    public static ArrayList<User> storageCollectionsUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(1L, "Roman", "123", Methods.generateSalt()));
+        users.add(new User(2L, "Roman1", "000", Methods.generateSalt()));
+        users.add(new User(3L, "Roman2", "0000", Methods.generateSalt()));
+
+        return users;
+    }
+
+    /**
+     * Коллекция пользователей их ресурсов и ролей
+     */
+    public static ArrayList<UserRes> storageCollectionsUserRes() {
+        ArrayList<UserRes> userRes = new ArrayList<>();
+        userRes.add(new UserRes(1L, 1L, "a.b", Roles.READ.toString()));
+        userRes.add(new UserRes(1L, 1L, "A.B.C.D", Roles.READ.toString()));
+        userRes.add(new UserRes(1L, 1L, "A.B.C.D", Roles.WRITE.toString()));
+        userRes.add(new UserRes(2L, 2L, "A.B.C.D", Roles.WRITE.toString()));
+        userRes.add(new UserRes(3L, 3L, "a.b", Roles.EXECUTE.toString()));
+
+        return userRes;
+    }
+
+    /**
+     * Аутентификация
+     */
+    public static void toAuthenticate(String log, String pass,
+                                      ArrayList<User> users) throws NoSuchAlgorithmException {
+        boolean isCheck = false;
+        for (User user : users) {
+             /*Проверка совпадает ли пароль с хранящимся в коллекции*/
+            if ((log.equals(user.getLogin())) && !Methods.isRightPass(pass, user.getPassword(), user.getSalt())) {
+                System.exit(2);
+            }
+
+            /*Проверка Существует ли логин*/
+            if ((log.equals(user.getLogin()))) {
+                isCheck = true;
+            }
+        }
+        if (!isCheck) {
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Авторизация
+     */
+    public static void toAuthorize(String log, String rol, String res,
+                                   ArrayList<User> users, ArrayList<UserRes> userRes) throws NoSuchAlgorithmException {
+        boolean isCheckRes = false;
+        for (User user : users) {
+            for (UserRes userRe : userRes) {
+                if (!rol.equals("READ") && !rol.equals("WRITE") && !rol.equals("EXECUTE")) {
+                    System.exit(3);
+                }
+
+                /*
+                  Проверка на существование запрашивамого ресурса у пользователя, если совпадений нет ,
+                  вызывается метод поиска дочерних ресурсов
+                 */
+                if (((log.equals(user.getLogin()))) && rol.equals(userRe.getRole())
+                        && Methods.checkChildPaths(userRe.getPath(), res)) {
+                    isCheckRes = true;
+                }
+            }
+        }
+        if (!isCheckRes) {
+            System.exit(4);
+        }
+    }
+
+    /**
+     * Аккаунтинг
+     */
+    public static void toAccounting(String ds, String de, String vol) throws NoSuchAlgorithmException {
+        /*Проверки валидности даты*/
+        if (!Methods.isCheckDate(ds)) {
+            System.exit(5);
+        }
+
+        if (!Methods.isCheckDate(de)) {
+            System.exit(5);
+        }
+
+        /*Проверка валидности объема*/
+        if (!Methods.isCheckValue(vol)) {
+            System.exit(5);
+        }
+    }
+
+    /**
+     * Генерация соли
+     */
+    private static String generateSalt() {
+        /*Получить 32битное значение */
+        final Random RANDOM = new SecureRandom();
+        byte[] s = new byte[32];
+        RANDOM.nextBytes(s);
+
+        /*Перевод в строку*/
+        BigInteger bigInt = new BigInteger(1, s);
+        return bigInt.toString(16);
+    }
+
+    /**
+     * Хэш пароля
+     */
+    private static String generateHashPassword(String str) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest;
+        /*Создание обьекта для использования алгоритма*/
+        messageDigest = MessageDigest.getInstance("MD5");
+        /*Сброс*/
+        messageDigest.reset();
+        /*Обновляет digest используя указанный байт*/
+        messageDigest.update(str.getBytes());
+
+        byte[] digest;
+
+        digest = messageDigest.digest();
+
+        //конвертируем байт в шестнадцатеричный формат
+        BigInteger bigInt = new BigInteger(1, digest);
+        StringBuilder md5Hex = new StringBuilder(bigInt.toString(16));
+
+        while (md5Hex.length() < 32) {
+            md5Hex.insert(0, "0");
+        }
+        return md5Hex.toString();
+    }
+
+    /**
+     * Поиск дочернего ресурса
+     */
+    private static boolean checkChildPaths(String rU, String tR) {
+        /*Разбитие строк на узлы*/
+        String[] resUser = rU.split("\\.");
+        String[] resTransf = tR.split("\\.");
+
+        /*Если длина переданного ресурса меньше длины ресурса пользователя возвращать false*/
+        if (resTransf.length < resUser.length) {
+            return false;
+        }
+
+        /*Если совпадений не найдено возвращать false*/
+        for (int i = 0; i < resUser.length; i++) {
+            if (!resUser[i].equals(resTransf[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Проверка совпадает ли переданный хэшированный пароль с паролем пользователя
+     */
+    private static boolean isRightPass(String pass, String userPass, String salt) throws NoSuchAlgorithmException {
+        return generateHashPassword(generateHashPassword(pass + salt)).equals(generateHashPassword(generateHashPassword(userPass + salt)));
+    }
+
+    /**
+     * Проверка валидности обьема
+     */
+    private static boolean isCheckValue(String volume) {
+        return volume.matches("^-?\\d+$");
+    }
+
+    /**
+     * Проверка валидности даты
+     */
+    private static boolean isCheckDate(String date) {
+        try {
+            LocalDate.parse(date);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
