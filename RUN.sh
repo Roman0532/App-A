884c866c178bb199f486bbc33b99b38545d372e9
Roman0532 <Roman62477@yandex.ru>
2017-11-07 20:15:36 +0700

    Check push

--- RUN.sh
+++ RUN.sh
@@ -3,6 +3,11 @@ LIB="lib/*"
 SRC="src/"
 OUT="out/classes/"
 
+sudoe() {
+[[ "$#" -ne 2 ]] && echo "Usage: sudoe <text> <file>" && return 1
+echo "$1" | sudo tee --append "$2" > /dev/null
+}
+
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     CP="$LIB"
 fi
