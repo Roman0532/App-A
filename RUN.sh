e15e52e506c38ac6ab39c5b05f635bbb8a14fbec
Roman0532 <32132937+Roman0532@users.noreply.github.com>
2017-11-08 00:06:45 +0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -6,11 +6,11 @@ OUT="out/classes/"
 chmod +x RUN.sh
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    CP="lib/*;out/classes/Main.jar;"
+    CP="lib/*;out/classes/Main.jar"
 	
 	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	
-    CP="lib/*;out/classes/Main.jar;"
+    CP="lib/*;out/classes/Main.jar"
 fi
 
 java -cp "$CP" Main $@
