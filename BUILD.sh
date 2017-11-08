683e6ef2a4c442ecb879d8849beb2f683b9d9eb5
Roman0532 <32132937+Roman0532@users.noreply.github.com>
2017-11-09 02:40:59 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 source ./CONFIG.sh
 
 rm -rf $OUT
 mkdir -p $OUT
 
+# Компиляция проекта
 find . -name "*.java" | xargs javac -sourcepath "SRC" -classpath "$CP" -d "$OUT" 
 
 jar -cfe $JAR Main -C $OUT .
