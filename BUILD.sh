bff14360967ca017e5c36c8191a65bc1ebff007e
Roman0532 <32132937+Roman0532@users.noreply.github.com>
2017-11-09 02:32:31 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-LIB="lib/*"
-SRC="src"
-OUT="out/classes"
-JAR="out/classes/Main.jar"
-
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    CP="$LIB"
-elif [ "$(expr substr $(uname -s) 1 10)" == "Linux" ]; then
-    CP="$LIB"
-fi
+source ./CONFIG.sh
 
 rm -rf $OUT
 mkdir -p $OUT
