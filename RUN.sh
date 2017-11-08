9a28d2e427fa40e254ce3b9a2b0601bb89ffcff0
Roman0532 <32132937+Roman0532@users.noreply.github.com>
2017-11-09 02:37:27 +0700

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-LIB="lib/*"
-SRC="src/"
-OUT="out/classes/"
-JAR="out/classes/Main.jar"
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    CP="$LIB;$JAR"
-	elif [ "$(expr substr $(uname -s) 1 10)" == "Linux" ]; then
-    CP="$LIB:$JAR"
-fi
+
+source ./CONFIG.sh
 
 java -cp "$CP" Main $@
