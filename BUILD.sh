b739d582669e1181fcfe19182102ac1b77942cd6
Roman0532 <Roman62477@yandex.ru>
2017-11-09 11:32:59 +0700

    Update TEST.sh

--- BUILD.sh
+++ BUILD.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-LIB="lib/*"
-SRC="src"
-OUT="out/classes"
-JAR="out/classes/Main.jar"
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    CP="$LIB;$JAR"
-elif [ "$(expr substr $(uname -s) 1 10)" == "Linux" ]; then
-    CP="$LIB:$JAR"
-fi
-
-rm -rf $OUT
-mkdir -p $OUT
-
-find . -name "*.java" | xargs javac -sourcepath "SRC" -classpath "$CP" -d "$OUT"
-
-jar -cfe $JAR Main -C $OUT .
-=======
 
 source ./CONFIG.sh
 
--- RUN.sh
+++ RUN.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-LIB="lib/*"
-SRC="src/"
-OUT="out/classes/"
-JAR="out/classes/Main.jar"
-
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    CP="$LIB;$JAR"
-elif [ "$(expr substr $(uname -s) 1 10)" == "Linux" ]; then
-    CP="$LIB:$JAR"
-fi
-=======
 
 source ./CONFIG.sh
 
