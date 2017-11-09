5f1ce752d9057473adde2551559693c604f2b934
Roman0532 <Roman62477@yandex.ru>
2017-11-09 11:14:00 +0700

    Add Plan 2

--- BUILD.sh
+++ BUILD.sh
@@ -4,16 +4,14 @@ SRC="src"
 OUT="out/classes"
 JAR="out/classes/Main.jar"
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    CP="$LIB"
-	
-	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	
-    CP="$LIB"
+    CP="$LIB;$JAR"
+elif [ "$(expr substr $(uname -s) 1 10)" == "Linux" ]; then
+    CP="$LIB:$JAR"
 fi
 
-rm -rf out/classes
-mkdir out/classes
+rm -rf $OUT
+mkdir -p $OUT
 
-find . -name "*.java" | xargs javac -sourcepath "SRC" -classpath "$CP" -d "$OUT" 
+find . -name "*.java" | xargs javac -sourcepath "SRC" -classpath "$CP" -d "$OUT"
 
-jar -cfe out/classes/Main.jar Main -C out/classes .
+jar -cfe $JAR Main -C $OUT .
