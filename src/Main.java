8ce7a9cbf1cce77a2cd390d25cd57abf4612083c
Roman0532 <32132937+Roman0532@users.noreply.github.com>
2017-11-22 15:46:18 +0700

    Add try-whis-resource p2 [#7]

--- src/Main.java
+++ src/Main.java
@@ -3,84 +3,77 @@ import dao.AuthenticationDao;
 import dao.AuthorizationDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.flywaydb.core.Flyway;
 import service.*;
 
 import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, org.apache.commons.cli.ParseException, SQLException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, org.apache.commons.cli.ParseException {
         int exitCode = 0;
 
         logger.debug("Приложение App-A запущено");
-        logger.debug("Выполнение миграций");
-
-        Flyway flyway = new Flyway();
-
-        flyway.setDataSource("jdbc:h2:./src/resources/db/migration/aaa", "sa", "");
-
-        flyway.migrate();
-
-
-        logger.debug("---Установка соеденения---");
-        ConnectionService connectionService = new ConnectionService();
-        java.sql.Connection dbConn = connectionService.getDbConnection();
-
-        if (dbConn != null) {
-            logger.debug("Соеденение прошло успешно");
-        } else {
-            logger.debug("Соеденение не удалось");
-        }
 
         //Передача аргументов парсеру
         ParseService cmd = new ParseService();
         UserData userData = cmd.parse(args);
 
-        AuthenticationDao authenticationDao = new AuthenticationDao(dbConn);
-        AuthorizationDao authorizationDao = new AuthorizationDao(dbConn, authenticationDao);
-        AccountingDao accountingDao = new AccountingDao(dbConn);
+        ConnectionService connectionService = new ConnectionService();
 
-        AuthenticationService authenticationService = new AuthenticationService(authenticationDao);
-        AuthorizationService authorizationService = new AuthorizationService(authorizationDao);
-        AccountingService accountingService = new AccountingService(accountingDao);
+        logger.debug("Выполнение миграций");
+        connectionService.DbMigration();
 
-        if (!userData.isAuthentication()) {
-            logger.debug("Параметров не передано,выполняется вывод справки");
-            cmd.printHelp();
-        }
+        try (Connection dbConn = connectionService.getDbConnection()) {
+            logger.debug("---Установка соеденения---");
+            if (dbConn != null) {
+                logger.debug("Соеденение прошло успешно");
 
-        if (userData.isAuthentication()) {
-            logger.debug("Передано 2 параметра {} {} , выполняется аутентификация",
-                    userData.getLogin(), userData.getPassword());
-            exitCode = authenticationService.authenticate(userData.getLogin(), userData.getPassword());
-        }
+                AuthenticationDao authenticationDao = new AuthenticationDao(dbConn);
+                AuthorizationDao authorizationDao = new AuthorizationDao(dbConn, authenticationDao);
+                AccountingDao accountingDao = new AccountingDao(dbConn);
 
-        if (userData.isAuthorization()) {
-            logger.debug("Передано 4 параметра {} {} {} {}, выполняется авторизация", userData.getLogin(),
-                    userData.getPassword(), userData.getRole(), userData.getResource());
-            if (exitCode == 0) {
-                exitCode = authorizationService.authorize(userData.getLogin(), userData.getRole(), userData.getResource());
+                AuthenticationService authenticationService = new AuthenticationService(authenticationDao);
+                AuthorizationService authorizationService = new AuthorizationService(authorizationDao);
+                AccountingService accountingService = new AccountingService(accountingDao);
+
+                if (!userData.isAuthentication()) {
+                    logger.debug("Параметров не передано,выполняется вывод справки");
+                    cmd.printHelp();
+                }
+
+                if (userData.isAuthentication()) {
+                    logger.debug("Передано 2 параметра {} {} , выполняется аутентификация",
+                            userData.getLogin(), userData.getPassword());
+                    exitCode = authenticationService.authenticate(userData.getLogin(), userData.getPassword());
+                }
+
+                if (userData.isAuthorization()) {
+                    logger.debug("Передано 4 параметра {} {} {} {}, выполняется авторизация", userData.getLogin(),
+                            userData.getPassword(), userData.getRole(), userData.getResource());
+                    if (exitCode == 0) {
+                        exitCode = authorizationService.authorize(userData.getLogin(), userData.getRole(), userData.getResource());
+                    }
+                }
+
+                if (userData.isAccounting()) {
+                    logger.debug("Передано 7 параметров {} {} {} {} {} {} {}, выполняется аккаунтинг", userData.getLogin(),
+                            userData.getPassword(), userData.getRole(), userData.getResource(),
+                            userData.getDataStart(), userData.getDataEnd(), userData.getVolume());
+                    if (exitCode == 0)
+                        exitCode = accountingService.accounting(userData.getDataStart(),
+                                userData.getDataEnd(), userData.getVolume(), userData);
+                }
+            } else {
+                logger.error("Соеденение не удалось");
+                System.exit(255);
             }
+        } catch (SQLException e) {
+            logger.error("Произошка ошибка БД", e);
         }
-
-        if (userData.isAccounting()) {
-            logger.debug("Передано 7 параметров {} {} {} {} {} {} {}, выполняется аккаунтинг", userData.getLogin(),
-                    userData.getPassword(), userData.getRole(), userData.getResource(),
-                    userData.getDataStart(), userData.getDataEnd(), userData.getVolume());
-            if (exitCode == 0)
-                exitCode = accountingService.accounting(userData.getDataStart(),
-                        userData.getDataEnd(), userData.getVolume(), userData);
-        }
-
-        if (dbConn != null) {
-            dbConn.close();
-            logger.debug("Соеденение закрыто");
-        }
-
         System.exit(exitCode);
     }
 }
