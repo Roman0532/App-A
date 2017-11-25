1dc773aeafed6aca5c2756664912a1762d0d10b2
Roman0532 <32132937+Roman0532@users.noreply.github.com>
2017-11-25 17:50:45 +0700

    Update DbException.java

--- src/service/DbException.java
+++ src/service/DbException.java
@@ -4,12 +4,4 @@ public class DbException extends Exception{
     public DbException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public DbException(String message) {
-        super(message);
-    }
-
-    public DbException(Throwable cause) {
-        super(cause);
-    }
 }
