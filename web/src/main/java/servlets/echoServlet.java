a18e168046a9222f1ad375387f9d7485e34f35d9
Roman0532 <Roman62477@yandex.ru>
2017-12-08 19:15:48 +0700

    add heroku

--- web/src/main/java/servlets/echoServlet.java
+++ web/src/main/java/servlets/echoServlet.java
@@ -13,15 +13,13 @@ public class echoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        String urlGet = ("http://localhost:8080/web-1.0-SNAPSHOT/echo/get");
         req.setCharacterEncoding("UTF-8");
-        if (req.getRequestURL().toString().equals(urlGet)) {
+
             out.println("<html>");
             out.println("<body>");
             out.println("<p>" + req.getParameter("id") + "</p>");
             out.println("</body>");
             out.println("</html>");
-        }
     }
 
     @Override
