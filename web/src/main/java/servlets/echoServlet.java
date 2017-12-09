bf6969f3139d3c64b73caff1d93cbfb022d5827a
Roman0532 <Roman62477@yandex.ru>
2017-12-09 20:29:48 +0700

    Update pom.xml

--- web/src/main/java/servlets/echoServlet.java
+++ web/src/main/java/servlets/echoServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class echoServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
