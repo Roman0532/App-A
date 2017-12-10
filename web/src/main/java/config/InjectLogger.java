df7abdf96365ef295303ed0b45128c2725f0ad8e
Roman0532 <Roman62477@yandex.ru>
2017-12-11 00:30:22 +0700

    add heroku

--- web/src/main/java/config/InjectLogger.java
+++ web/src/main/java/config/InjectLogger.java
@@ -1,11 +1,9 @@
 package config;
 
-import javax.inject.Scope;
 import java.lang.annotation.*;
-@Scope
+
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
 public @interface InjectLogger {
 }
